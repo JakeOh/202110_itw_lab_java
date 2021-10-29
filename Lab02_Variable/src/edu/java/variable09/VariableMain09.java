3e27e79345b378d98213e59b4744716f601a93b4
TEA <TEA@DESKTOP-FFDKC0U>
2021-10-29 17:20:40 +0900

    .

---
+++ Lab02_Variable/src/edu/java/variable09/VariableMain09.java
@@ -0,0 +1,14 @@
+package edu.java.variable09;
+
+public class VariableMain09 {
+
+	public static void main(String[] args) {
+		// 간단한 성적 처리 프로그램
+		// Java, SQL, JSP 과목의 점수(int)를 Scanner를 사용해서 입력받음.
+		// 세 과목의 점수를 출력
+		// 세 과목의 총점을 출력
+		// 세 과목의 평균을 출력 - 소수점이 계산되도록!
+
+	}
+
+}
