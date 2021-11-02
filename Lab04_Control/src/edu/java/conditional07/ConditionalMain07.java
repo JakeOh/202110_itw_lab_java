c5fd714bb60613130c8260a9e879e93815c4cf75
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-02 11:51:17 +0900

    .

---
+++ Lab04_Control/src/edu/java/conditional07/ConditionalMain07.java
@@ -0,0 +1,17 @@
+package edu.java.conditional07;
+
+public class ConditionalMain07 {
+
+	public static void main(String[] args) {
+		// 1.
+		// 0 이상 10 이하의 정수 난수 2개를 만들어서 변수에 저장.
+		// 두 숫자의 차이(큰 수 - 작은 수)를 차이를 출력.
+
+		
+		// 2.
+		// 0 이상 10 미만의 정수 난수를 1개를 만들어서 변수에 저장.
+		// 정수가 짝수인 지 홀수인 지를 출력.
+		
+	}
+
+}
