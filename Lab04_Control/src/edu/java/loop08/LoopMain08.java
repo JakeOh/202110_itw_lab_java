9f45b1a035bb7ce6ae42fa24fb1036949d10e5b4
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-03 17:30:26 +0900

    .

---
+++ Lab04_Control/src/edu/java/loop08/LoopMain08.java
@@ -0,0 +1,23 @@
+package edu.java.loop08;
+
+public class LoopMain08 {
+
+	public static void main(String[] args) {
+		/* 구구단을 1단부터 12단까지 출력. 1단은 1x1까지, 2단은 2x2까지, ..., 12단은 12x12까지 출력하세요.
+			break를 사용하는 방법과 break를 사용하지 않는 방법을 생각해 보세요.
+			(출력 결과)
+			1 x 1 = 1
+			----------
+			2 x 1 = 2
+			2 x 2 = 4
+			----------
+			3 x 1 = 3
+			3 x 2 = 6
+			3 x 3 = 9
+			----------
+			...
+		 */
+		
+	}
+
+}
