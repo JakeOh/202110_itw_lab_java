16abc342b3720c0076bb857c989b259b74f411c4
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-04 10:26:57 +0900

    .

--- Lab04_Control/src/edu/java/loop08/LoopMain08.java
+++ Lab04_Control/src/edu/java/loop08/LoopMain08.java
@@ -18,6 +18,32 @@ public class LoopMain08 {
 			...
 		 */
 		
-	}
+		// break 사용
+		for (int x = 1; x <= 12; x++) {
+			System.out.println(x + "단");
+			
+			for (int y = 1; y <= 12; y++) {
+				System.out.println(x + " x " + y + " = " + (x * y));
+				if (x == y) {  // 단과 곱해주는 숫자가 같은 경우
+					break;     // 구구단 출력을 멈춤
+				}
+			}  // end for(y)
+			
+			System.out.println("----------");
+		}  // end for(x)
+		
+		System.out.println();
+		// break 사용하지 않는 방법
+		for (int x = 1; x <= 12; x++) {
+			System.out.println(x + "단");
+			
+			for (int y = 1; y <= x; y++) {
+				System.out.println(x + " x " + y + " = " + (x * y));
+			}  // end for(y)
+			
+			System.out.println("----------");
+		}  // end for(x)
+		
+	}  // end main
 
-}
+}  // end class
