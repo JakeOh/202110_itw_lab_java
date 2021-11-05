77f6ae2e48affbfa833d5626c1995407e2d5dbf3
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-05 18:01:15 +0900

    .

--- Lab05_Array/src/edu/java/array08/ArrayMain08.java
+++ Lab05_Array/src/edu/java/array08/ArrayMain08.java
@@ -6,15 +6,14 @@ public class ArrayMain08 {
 
 	public static void main(String[] args) {
 		// 성적 처리 프로그램
-		
-		Random rand = new Random();  // 난수를 이용해서 점수를 만듦.
+		Random rand = new Random();  // 난수를 이용해서 점수를 만들기 위해서.
 		
 		// 0 ~ 10 사이의 난수 5개를 저장할 수 있는 배열 선언.
 		// 배열에 난수 5개를 저장.
 		// 배열에 저장된 모든 점수를 한 줄에 출력.
-		// 총점을 계산하고 출력
-		// 평균을 계산하고 출력
-		// 표준편차를 계산하고 출력
+		// 총점을 계산하고 출력: sum = 0 + scores[0] + scores[1] + scores[2] + ...
+		// 평균을 계산하고 출력: (double)sum / length
+		// 표준편차를 계산하고 출력: Math.sqrt() 메서드를 이용 -> 제곱근(squared root)
 		// 최댓값을 찾아서 출력
 		// 최솟값을 찾아서 출력
 
