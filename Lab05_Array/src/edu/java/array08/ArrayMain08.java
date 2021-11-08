57756b5a39b7e71a2ceb4ba0760c65e857e5b8cf
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-08 11:23:42 +0900

    .

--- Lab05_Array/src/edu/java/array08/ArrayMain08.java
+++ Lab05_Array/src/edu/java/array08/ArrayMain08.java
@@ -8,12 +8,12 @@ public class ArrayMain08 {
 		// 성적 처리 프로그램
 		Random rand = new Random();  // 난수를 이용해서 점수를 만들기 위해서.
 		
-		// 0 ~ 10 사이의 난수 5개를 저장할 수 있는 배열 선언.
-		int[] scores = new int[5];
+		// 0 ~ 100 사이의 난수 10개를 저장할 수 있는 배열 선언.
+		int[] scores = new int[10];
 		
-		// 배열에 난수 5개를 저장.
+		// 배열에 난수 10개를 저장.
 		for (int i = 0; i < scores.length; i++) {
-			scores[i] = rand.nextInt(11);
+			scores[i] = rand.nextInt(101);
 		}
 		
 		// 배열에 저장된 모든 점수를 한 줄에 출력.
@@ -49,7 +49,22 @@ public class ArrayMain08 {
 		System.out.println("표준편차: " + stdDev);
 		
 		// 최댓값을 찾아서 출력
+		int max = scores[0];  // 배열의 첫번째 원소를 최댓값이라고 가정
+		for (int i = 1; i < scores.length; i++) {  // 배열의 원소들을 모두 반복하면서
+			if (max < scores[i]) {  // 배열에서 max보다 더 큰 값을 찾으면
+				max = scores[i];  // max를 배열에서 찾은 값으로 바꿈.
+			}
+		}
+		System.out.println("최댓값: " + max);
+		
 		// 최솟값을 찾아서 출력
+		int min = scores[0];  // 최솟값을 배열의 첫번째 값이라고 가정
+		for (int s : scores) {  // 배열의 모든 원소들에 대해서 반복하면서
+			if (min > s) {  // 배열에서 min보다 더 작은 값을 발견했다면
+				min = s;  // 더 작은 값으로 min을 변경
+			}
+		}
+		System.out.println("최솟값: " + min);
 
 	}
 
