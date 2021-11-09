e0a74e67b9a695485b3b5db62d813b3250fb4e27
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-09 12:20:32 +0900

    .

---
+++ Lab07_Class/src/edu/java/class01/BasicTv.java
@@ -0,0 +1,14 @@
+package edu.java.class01;
+
+// 객체 지향 프로그래밍(OOP: Object-Oriented Programming) 언어: Java, C++, C#, Python, ...
+// 클래스(class): 프로그램에서 필요한 데이터(field)와 기능(method)을 묶어서 하나의 "데이터 타입"을 정의한 것.
+// 데이터 타입 -> 변수 선언, 파마미터 선언, 리턴 타입 선언
+
+public class BasicTv {
+
+	// 데이터(객체의 상태를 저장하기 위한 변수) - field, instance variable(인스턴스 변수)
+	boolean powerOn;  // TV의 현재 전원 상태를 저장하기 위한 필드(true: ON, false: OFF)
+	int volume; // TV의 현재 음량 상태를 저장하기 위한 필드
+	int channel;  // TV의 현재 채널 번호를 저장하기 위한 필드
+
+}
