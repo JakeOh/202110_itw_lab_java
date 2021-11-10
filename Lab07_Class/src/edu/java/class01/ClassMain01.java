67ed06741f30855db5cb2c031d3ff283b604527b
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-10 11:25:32 +0900

    .

--- Lab07_Class/src/edu/java/class01/ClassMain01.java
+++ Lab07_Class/src/edu/java/class01/ClassMain01.java
@@ -10,6 +10,8 @@ public class ClassMain01 {
 		// BasicTv 클래스 타입의 변수 선언, BasicTv 인스턴스 생성하고 선언한 변수에 할당(저장)
 		BasicTv tv1 = new BasicTv();
 		System.out.println(tv1);
+		// 지역 변수 tv1은 BasicTv 인스턴스가 생성된 메모리의 주소(참조) 값을 저장하고 있음.
+		
 		tv1.info();
 		// 메모리에 객체가 생성될 때 클래스의 field들은 그 데이터 타입의 기본값으로 초기화됨.
 		// 데이터 타입 기본값: boolean(false), int(0), double(0.0), ...
