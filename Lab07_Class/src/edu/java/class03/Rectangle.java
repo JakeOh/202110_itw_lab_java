7356bd3eeb067f374858a3bffd999ddd22b012a1
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-10 16:55:36 +0900

    .

---
+++ Lab07_Class/src/edu/java/class03/Rectangle.java
@@ -0,0 +1,17 @@
+package edu.java.class03;
+
+public class Rectangle {
+	// field - 속성, 데이터
+	double width;   // 직사각형의 가로 길이
+	double height;  // 직사각형의 세로 길이
+	
+	// constructor(생성자)
+	// 1) default constructor: width와 height를 모두 0.0으로 초기화
+	// 2) 파라미터가 1개인 생성자: width와 height를 같은 값으로 초기화
+	// 3) 파라미터가 2개인 생성자: 첫번째 argument로 width를 초기화, 두번째 argument로 height를 초기화
+	
+	// method - 기능
+	// 1) 직사각형의 넓이를 리턴하는 메서드
+	// 2) 직사각형의 둘레 길이를 리턴하는 메서드
+
+}
