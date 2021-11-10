392d77d85be9d3943d1ba692763f8c4abaae2927
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-10 15:22:14 +0900

    .

--- Lab07_Class/src/edu/java/class02/Student.java
+++ Lab07_Class/src/edu/java/class02/Student.java
@@ -7,4 +7,18 @@ public class Student {
 	// field: "학생" 객체가 가져야 할 속성/정보
 	int id;  // 학번
 	String name;  // 이름
-}
+	
+	// 생성자(constructor)
+	// 1) default constructor(기본 생성자): 파라미터가 없는 생성자
+	public Student() {
+		// 할 일 없음.
+	}
+	
+	// 2) 파라미터를 갖는 생성자
+	public Student(int id, String name) {
+		// this.field = parameter; 생성자 호출하는 곳에서 파라미터에 전달 값을 필드에 저장(초기화).
+		this.id = id;
+		this.name = name;
+	}
+	
+} // end class Student
