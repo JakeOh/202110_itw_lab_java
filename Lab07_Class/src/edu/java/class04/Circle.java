cb14d09d34a9dc351c170b743a27145f3b62ca2e
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-11 16:21:31 +0900

    .

--- Lab07_Class/src/edu/java/class04/Circle.java
+++ Lab07_Class/src/edu/java/class04/Circle.java
@@ -6,11 +6,28 @@ package edu.java.class04;
 // 기능: 넓이(3.14 * r^2), 둘레의 길이(2 * Math.PI * r), 원의 중심을 이동, info
 public class Circle {
 	// field
-	Point center;
+	Point center;  // 클래스 이름은 '타입'이기 때문에 변수를 선언할 때 사용 가능.
 	double radius;
 	
 	// 생성자
+	// default 생성자: 모든 필드를 그 필드의 기본값으로 초기화 한다.
+	public Circle() {
+		center = new Point();  // center가 null이 되지 않도록 하기 위해서.
+	}
+	
+	public Circle(Point center, double radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+	
+	public Circle(double x, double y, double r) {
+		this.center = new Point(x, y);
+		this.radius = r;
+	}
 	
 	// 메서드
+	public void info() {
+		System.out.println("Circle(중심 x=" + this.center.x + ", 중심 y=" + this.center.y + ", 반지름=" + this.radius +")");
+	}
 	
-}
+}  // end class Circle
--- Lab07_Class/src/edu/java/class04/ClassMain04.java
+++ Lab07_Class/src/edu/java/class04/ClassMain04.java
@@ -27,6 +27,25 @@ public class ClassMain04 {
 		// 위의 (1)과 (2)를 나눠서 호출하는 코드를 아래와 같은 메서드 연쇄 호출로 할 수 있다.
 		// pt2.move(2, 1).move(0,2);
 
+		System.out.println();
+		System.out.println("--------------------");
+		
+		Circle c1 = new Circle();
+		System.out.println(c1.radius);
+		System.out.println(c1.center);  // null: Point 인스턴스(center)가 아직 생성되지 않았음.
+		System.out.println(c1.center.x);  // NullPointerException 해결
+		c1.info();
+		
+		System.out.println();
+		Point center = new Point();  // 원점(0, 0)을 생성
+		Circle c2 = new Circle(center, 10.0);  // 중심이 원점에 있고 반지름이 10인 원을 생성
+		// new Circle(new Point(), 10.0); 와 동일.
+		c2.info();
+		
+		System.out.println();
+		Circle c3 = new Circle(0, 0, 1);
+		c3.info();
+		
 	}  // end main()
 
-}
+}  // end class ClassMain04
