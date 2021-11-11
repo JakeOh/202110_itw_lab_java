ba868fe2560ba7297b2bcd129a1cb83709e69d34
TEA <TEA@DESKTOP-FFDKC0U>
2021-11-11 11:17:58 +0900

    .

--- Lab07_Class/src/edu/java/class03/Rectangle.java
+++ Lab07_Class/src/edu/java/class03/Rectangle.java
@@ -1,7 +1,7 @@
 package edu.java.class03;
 
 public class Rectangle {
-	// field - 속성, 데이터
+	// field(멤버 변수) - 속성, 데이터
 	double width;   // 직사각형의 가로 길이
 	double height;  // 직사각형의 세로 길이
 	
@@ -12,8 +12,9 @@ public class Rectangle {
 	}
 	// 2) 파라미터가 1개인 생성자: width와 height를 같은 값으로 초기화
 	public Rectangle(double length) {
-		this.width = length;
-		this.height = length;
+//		this.width = length;
+//		this.height = length;
+		this(length, length);  // 파라미터 2개인 생성자 호출
 	}
 	// 3) 파라미터가 2개인 생성자: 첫번째 argument로 width를 초기화, 두번째 argument로 height를 초기화
 	public Rectangle(double width, double height) {
@@ -23,6 +24,13 @@ public class Rectangle {
 	
 	// method - 기능
 	// 1) 직사각형의 넓이를 리턴하는 메서드
+	public double area() {
+		return width * height;
+	}
+	
 	// 2) 직사각형의 둘레 길이를 리턴하는 메서드
+	public double perimeter() {
+		return (width + height) * 2;
+	}
 
 }
---
+++ Lab07_Class/src/edu/java/class04/Point.java
@@ -0,0 +1,13 @@
+package edu.java.class04;
+
+// 2차원 평면 상의 점의 좌표를 표현하는 클래스
+public class Point {
+	// 필드(멤버 변수)
+	double x;  // 점의 x 좌표
+	double y;  // 점의 y 좌표
+	
+	// 생성자 - default, 파라미터 2개
+	
+	// 메서드 - 점의 좌표를 dx, dy만큼 이동시키는 기능
+	
+}
