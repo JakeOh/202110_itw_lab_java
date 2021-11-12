37c50453113a5f3c77e07060ee5386fe745eef91
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-12 17:37:12 +0900

    .

---
+++ Lab07_Class/src/edu/java/modifier05/Member.java
@@ -0,0 +1,14 @@
+package edu.java.modifier05;
+
+public class Member {
+	// field
+	private String memberId;
+	private String memberPassword;
+	
+	// constructor - 파라미터 2개인 생성자만 만듦. 기본 생성자는 만들지 않음.
+	
+	// 비밀번호에 대한 getter 메서드 작성
+	
+	// 아디이와 비밀번호에 대한 setter 메서드 작성
+	
+}
