8ff1da1f98ba6679d3b29b4d2e39a457db390517
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-12 15:38:20 +0900

    .

--- Lab07_Class/src/edu/java/modifier02/ModifierMain02.java
+++ Lab07_Class/src/edu/java/modifier02/ModifierMain02.java
@@ -3,7 +3,7 @@ package edu.java.modifier02;
 import edu.java.modifier01.AccessTest;
 // 같은 패키지에 있는 클래스는 import문 없이 변수 선언할 때 클래스 이름을 사용할 수 있음.
 // 다른 패키지에 있는 클래스로 변수를 선언할 때는
-// (1) import문을 작성하고 클래스 이름을 사용.
+// (1) import문을 작성하고 클래스 이름을 사용하거나
 // (2) 패키지를 포함한 클래스의 full name을 사용.
 // 단, java.lang 패키지의 모든 클래스는 import문 없이 사용 가능 - (예) String
 
