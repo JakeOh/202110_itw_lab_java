171050e111faf55d4f67476c39e367ec40f5e8b2
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-15 11:32:08 +0900

    .

--- Lab07_Class/src/edu/java/modifier07/StaticTest.java
+++ Lab07_Class/src/edu/java/modifier07/StaticTest.java
@@ -6,11 +6,11 @@ package edu.java.modifier07;
 
 // static 멤버(변수/필드, 메서드): static 수식어를 사용한 필드, 메서드.
 // - 인스턴스가 생성되지 않아도 사용할 수 있는 멤버 변수(필드), 메서드.
-// - static 멤버들은 메모리 영역 중에서 메서드 영역(method area)에 생성됨.
+// - 프로그램이 시작될 때 클래스 로더(class loader)에 의해 메모리 영역 중에서 메서드 영역(method area)에 생성됨.
 // - "클래스이름.변수이름", "클래스이름.메서드이름(...)" 형식으로 사용.
 // - (예) Math.PI, Math.sqrt()
 
 
 public class StaticTest {
-
+	
 }
