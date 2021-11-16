d458667fd1ba57429e148eb9d0b8261332ea7f24
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-16 12:17:54 +0900

    .

--- Lab00_Contact/src/edu/java/contact/ver01/ContactMain01.java
+++ Lab00_Contact/src/edu/java/contact/ver01/ContactMain01.java
@@ -27,8 +27,10 @@ public class ContactMain01 {
 				run = false;
 				break; // switch를 끝냄.
 			case 1: // 전체 검색 - 배열에 저장된 모든 연락처 출력
+				selectAllContacts();
 				break;
 			case 2: // 인덱스 검색 - 검색하고자 하는 인덱스의 연락처 1개만 출력
+				selectContactByIndex();
 				break;
 			case 3: // 연락처 추가
 				insertConctact();
@@ -36,15 +38,50 @@ public class ContactMain01 {
 			case 4: // 연락처 업데이트
 				break;
 			default:
+				System.out.println("메뉴를 다시 선택하세요...");
 			} // end switch
 		} // end while
 
 		System.out.println("*** 프로그램 종료***");
 	} // end main()
 
-	private static void insertConctact() {
+	private static void selectContactByIndex() {
 		System.out.println();
-		System.out.println("----- 연락처 추가 -----");
+		System.out.println("----- 인덱스 검색 기능 -----");
+		System.out.println("검색할 인덱스>>>");
+		int index = scanner.nextInt();  // 정수 입력
+		scanner.nextLine(); // 정수 다음에 입력된 "엔터"를 버퍼에서 제거하기 위해서
+		
+		if (index < MAX_LENGTH) {
+			System.out.println(contacts[index]);
+		} else {
+			System.out.println("존재하지 않는 인덱스입니다.");
+		}
+		System.out.println("------------------------------");
+		
+	} // end selectContactByIndex()
+
+	private static void selectAllContacts() {
+		System.out.println();
+		System.out.println("----- 연락처 전체 리스트 -----");
+//		for (Contact c : contacts) {
+//			System.out.println(c);
+//		} // null도 출력
+		for (int i = 0; i < count; i++) {
+			System.out.println(contacts[i]);
+		}
+		System.out.println("------------------------------");
+		
+	} // selectAllContacts()
+
+	private static void insertConctact() {
+		if (count >= MAX_LENGTH) {
+			System.out.println("저장 공간이 부족합니다!");
+			return;  // 메서드 종료
+		}
+		
+		System.out.println();
+		System.out.println("----- 새 연락처 추가 -----");
 		System.out.println("이름 입력>>>");
 		String name = scanner.nextLine(); // 콘솔 창에서 "엔터 키"가 입력될 때까지 입력된 모든 문자열을 입력받음.
 		System.out.println("전화번호 입력>>>");
@@ -59,6 +96,7 @@ public class ContactMain01 {
 		// 배열에 저장된 연락처의 개수를 1 증가시킴.
 		count++;
 		
+		System.out.println("새 연락처 추가 성공(" + count + ")");
 	} // end insertContact()
 
 	private static void showMainMenu() {
