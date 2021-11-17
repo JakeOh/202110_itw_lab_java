457797e180d0700bb8435e3bf56c1e68674bc8c9
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-17 10:33:19 +0900

    .

--- Lab08_Inheritance/src/edu/java/inheritance03/InheritanceMain03.java
+++ Lab08_Inheritance/src/edu/java/inheritance03/InheritanceMain03.java
@@ -9,10 +9,19 @@ public class InheritanceMain03 {
 		Car car1 = new Car(50);
 		car1.drive();
 		
+		System.out.println();
+		
 		// ElectricCar 클래스의 인스턴스를 생성
 		ElectricCar car2 = new ElectricCar(50, 100);
 		car2.drive();
 
+		// 다형성(polymorphism): 하나의 인스턴스를 여러가지 타입으로 부를 수 있는 것.
+		// 인스턴스를 자식 타입으로 생성하고, 그 인스턴스를 가리키는 참조 변수를 
+		// 자식 타입으로도, 부모 타입으로도 선언할 수 있는 것.
+		// line 15의 의미는 "참조 변수 car2는 ElectricCar 타입이다."
+		
+		Car car3 = new ElectricCar(0, 0); // "참조 변수 car3는 (실체는 ElectricCar이지만) Car 타입이다."는 의미.
+		
 	}
 
 }
