16d0f0ab9c27bd0da1f7b911359ee25f75573281
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-17 17:33:00 +0900

    .

---
+++ Lab08_Inheritance/src/edu/java/inheritance09/InheritanceMain09.java
@@ -0,0 +1,16 @@
+package edu.java.inheritance09;
+
+public class InheritanceMain09 {
+
+	public static void main(String[] args) {
+		// Shape 타입 2개를 저장할 수 있는 배열을 선언
+		
+		// Rectangle 객체를 생성하고 배열의 첫번째 원소로 저장
+		
+		// Circle 객체를 생성하고 배열의 두번째 원소로 저장
+		
+		// 향상된 for 문을 사용해서 배열의 원소들의 area() 메서드를 호출하고 결과 확인
+
+	}
+
+}
