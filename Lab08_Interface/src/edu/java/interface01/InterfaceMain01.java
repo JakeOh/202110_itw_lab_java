2c3e69c30e8bbe3ebc67ca2e109b6bdad38392e3
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-18 12:16:06 +0900

    .

--- Lab08_Interface/src/edu/java/interface01/InterfaceMain01.java
+++ Lab08_Interface/src/edu/java/interface01/InterfaceMain01.java
@@ -11,10 +11,21 @@ package edu.java.interface01;
 // 클래스는 단일 상속만 가능하지만, 인터페이스는 구현하는 개수 제한이 없음.
 
 public class InterfaceMain01 {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) {
+		// 구현된 인터페이스의 기능들을 사용
+		// DatabaseModule.DB_VERSION = 2;  // 인터페이스의 멤버 변수는 public static final
+
+		// 인터페이스이름 변수 = new 구현클래스생성자();
+//		DatabaseModule db = new OracleDatabaseModule();  // 다형성(polymorphism)
+		
+		// 데이터베이스 모듈이 Oracle에서 MySQL로 바뀐 경우...
+		DatabaseModule db = new MySQLDatabaseModule();
+		
+		// select와 update와 같은 메서드를 사용하는 방법은 바뀌지 않음.
+		db.select();
+		db.update("!@#$");
+		
 	}
 
 }
