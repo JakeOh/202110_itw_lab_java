7af453db5c33f6200b7516669ee1ecabbba3fe09
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-19 11:18:39 +0900

    .

---
+++ Lab11_MVC/src/edu/java/member/view/MemberMain.java
@@ -0,0 +1,18 @@
+package edu.java.member.view;
+
+// MVC(Model-View-Controller) 디자인 패턴
+// Model: 데이터 저장을 위한 클래스. VO(Value Object), DTO(Data Transfer Object).
+// - edu.java.member.model.Member
+// View: UI(User Interface)를 담당하는 클래스.
+// - edu.java.member.view.MemberMain
+// Controller: 프로그램의 특정 로직(기능, 비즈니스 로직)을 담당하는 클래스. DAO(Data Access Obejct).
+// - edu.java.member.controller.MemberDao, MemberDaoImpl
+
+public class MemberMain {
+
+	public static void main(String[] args) {
+		// 회원 관리 프로그램 메인(View)
+
+	}
+
+}
