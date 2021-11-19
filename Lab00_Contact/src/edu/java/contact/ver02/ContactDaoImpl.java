643f9eaa3e4b9beb02900f9cafb7149ae9911b43
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-19 17:56:01 +0900

    .

---
+++ Lab00_Contact/src/edu/java/contact/ver02/ContactDaoImpl.java
@@ -0,0 +1,31 @@
+package edu.java.contact.ver02;
+
+import edu.java.contact.model.Contact;
+
+public class ContactDaoImpl implements ContactDao {
+
+	@Override
+	public Contact[] select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact select(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(Contact c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(int index, Contact contact) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
