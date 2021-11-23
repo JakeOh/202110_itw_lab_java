2d6749f001e449504defa4b46d69fb97dff4d31b
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-23 14:19:01 +0900

    .

---
+++ Lab13_Collection/src/edu/java/list01/ListMain01.java
@@ -0,0 +1,34 @@
+package edu.java.list01;
+
+/*
+ * Collection<E>
+ * |__ List<E>
+ *     |__ ArrayList<E>, LinkedList<E>
+ * 
+ * 리스트(list)의 특징:
+ * 1) 값들이 저장되는 순서가 중요 - index를 가지고 있음.
+ * 2) 값들을 저장할 때마다 index가 자동으로 증가.
+ * 3) 리스트 중간에 있는 값을 삭제하면 인덱스가 자동으로 재배열됨.
+ * 4) 같은 값들을 여러번 저장할 수 있음.
+ * 5) 배열(array)를 좀 더 편리하게 사용할 수 있도록 여러가지 메서드들을 제공하는 클래스 
+ * 6) <E>는 리스트가 저장하는 데이터 타입을 의미 - 반드시 클래스 이름으로만 사용 가능
+ * 
+ * ArrayList<E>의 특징
+ * 1) 데이터를 저장(add)하는 속도가 느림
+ * 2) 데이터를 삭제(remove)하는 속도가 느림
+ * 3) 데이터를 검색(get)하는 속도가 매우 빠름
+ * 
+ * LinkedList<E>의 특징
+ * 1) 데이터를 저장(add)하는 속도가 빠름
+ * 2) 데이터를 삭제(remove)하는 속도가 빠름
+ * 3) 데이터를 검색(get)하는 속도가 느림
+ */
+
+public class ListMain01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
