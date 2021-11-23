5e3336278a4b0e51bfe0e4eb12e3d7c77d303ebb
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-23 16:55:39 +0900

    .

---
+++ Lab13_Collection/src/edu/java/list03/ListMain03.java
@@ -0,0 +1,27 @@
+package edu.java.list03;
+
+import java.util.Scanner;
+
+public class ListMain03 {
+
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// Member 타입을 저장할 수 있는 ArrayList를 생성
+		
+		// 반복문을 사용해서 
+		// 아이디와 비밀번호를 입력받아서 Member 객체를 생성해서
+		// ArrayList에 Member 타입 객체 3개를 추가
+		
+		// 리스트의 내용을 출력 - 각각의 회원의 아이디와 비밀번호가 출력될 수 있도록
+		
+		// 인덱스 0 위치의 회원 정보 중에서 비밀번호를 변경하고 리스트 내용 출력
+		
+		// 인덱스 1 위치의 회원 정보를 삭제하고 리스트 내용을 출력
+		
+		// 아이디가 일치하면 같은 회원으로 간주되도록 Member 클래스를 수정
+		// 아이디가 일치하는 회원을 리스트에서 삭제하고 리스트 내용을 출력
+		
+	}
+
+}
---
+++ Lab13_Collection/src/edu/java/list03/Member.java
@@ -0,0 +1,11 @@
+package edu.java.list03;
+
+public class Member {
+	// field
+	private String memberId;
+	private String memberPassword;
+	
+	// TODO: 생성자 - 기본 생성자, 파라미터 2개인 생성자
+	
+	// TODO: getters & setters
+}
