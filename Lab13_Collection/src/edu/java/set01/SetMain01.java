6eb61e6861e1e0fe5d2afc02f8ab9aa0103b1f7d
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-25 17:01:17 +0900

    .

--- Lab13_Collection/src/edu/java/set01/SetMain01.java
+++ Lab13_Collection/src/edu/java/set01/SetMain01.java
@@ -1,5 +1,8 @@
 package edu.java.set01;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /*
  * Collection<E>
  * |__ Set<E>
@@ -17,8 +20,42 @@ package edu.java.set01;
 public class SetMain01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// String을 저장하는 TreeSet을 생성
+		Set<String> set = new TreeSet<>();
+		// 변수를 선언할 때 Set이 저장하는 원소 타입을 생략할 순 없지만,
+		// 생성자를 호출할 때는 원소 타입을 생략할 수 있음.
+		
+		System.out.println("size: " + set.size());
+		System.out.println(set);
+		
+		// List<E>는 add(Object), remove(int), remove(Object), get(int), set(int, Object) 메서드를 가지고 있지만,
+		// Set<E>은 add(Object), remove(Object) 메서드만 가지고 있음. 인덱스(int)를 이용하는 메서드는 없음.
 
+		// Set<E>에 원소 추가 - add 
+		set.add("hello");
+		System.out.println("size: " + set.size());
+		System.out.println(set);
+		
+		set.add("hello");
+		System.out.println(set); // -> Set<E>은 중복된 값을 저장하지 않음.
+		
+		set.add("apple");
+		System.out.println(set);
+		
+		set.add("banana");
+		System.out.println(set);
+		// -> add 순서와 println에서 출력되는 원소들의 순서가 다름! 원소들이 알파벳 순서로 정렬되어 있음.
+		
+		// Set<E>이 가지고 있는 원소 삭제 - remove
+		set.remove("banana");
+		System.out.println(set);
+		
+		// Set<E>도 향상된 for 구문을 사용할 수 있음.
+		// Set<E>은 for (int i = 0; i < size; i++) {} 구문을 사용할 수 없음!
+		for (String s : set) {
+			System.out.println(s);
+		}
+		
 	}
 
 }
