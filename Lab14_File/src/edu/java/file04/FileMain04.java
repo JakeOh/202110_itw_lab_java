4f21305f69cdaeeadca86a847842dfe088676da9
Jake Oh <jake.oh.lecture@gmail.com>
2021-11-30 11:22:32 +0900

    .

---
+++ Lab14_File/src/edu/java/file04/FileMain04.java
@@ -0,0 +1,20 @@
+package edu.java.file04;
+
+/*
+ * 프로그램 <== BufferedInputStream <== FileInputStream <== 파일(HDD, SSD)
+ *   - FileInputStream: 하드디스크에 있는 파일을 읽어서 메모리(RAM)에 적재.
+ *   - BufferedInputStream: 메모리(RAM)에 올라가 있는 파일 내용을 읽음.
+ * 
+ * 프로그램 ==> BufferedOutputStream ==> FileOutputStream ==> 파일(HDD, SSD)
+ *   - BufferefOutputStream: 프로그램에서 메모리(RAM)에 내용을 쓰기(write).
+ *   - FileOutputStream: 하드디스크의 파일에 내용을 쓰기(write).
+ */
+
+public class FileMain04 {
+
+	public static void main(String[] args) {
+		// 파일의 내용을 메모리에 올려놓고 메모리에서 읽기/쓰기
+
+	}
+
+}
