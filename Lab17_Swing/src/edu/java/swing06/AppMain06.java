0b577c64c8e5be3924166e6277927f7ac30e5635
Jake Oh <jake.oh.lecture@gmail.com>
2021-12-07 16:45:01 +0900

    .

---
+++ Lab17_Swing/src/edu/java/swing06/AppMain06.java
@@ -0,0 +1,106 @@
+package edu.java.swing06;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
+public class AppMain06 {
+
+	private JFrame frame;
+	private JTextField textName;
+	private JTextField textPhone;
+	private JTextField textEmail;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppMain06 window = new AppMain06();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppMain06() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 560, 559);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel labelName = new JLabel("이름");
+		labelName.setOpaque(true);
+		labelName.setBackground(SystemColor.activeCaption);
+		labelName.setHorizontalAlignment(SwingConstants.CENTER);
+		labelName.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		labelName.setBounds(12, 10, 160, 64);
+		frame.getContentPane().add(labelName);
+		
+		textName = new JTextField();
+		textName.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		textName.setBounds(184, 10, 348, 64);
+		frame.getContentPane().add(textName);
+		textName.setColumns(10);
+		
+		JLabel labelPhone = new JLabel("전화번호");
+		labelPhone.setOpaque(true);
+		labelPhone.setHorizontalAlignment(SwingConstants.CENTER);
+		labelPhone.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		labelPhone.setBackground(SystemColor.activeCaption);
+		labelPhone.setBounds(12, 84, 160, 64);
+		frame.getContentPane().add(labelPhone);
+		
+		textPhone = new JTextField();
+		textPhone.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		textPhone.setColumns(10);
+		textPhone.setBounds(184, 84, 348, 64);
+		frame.getContentPane().add(textPhone);
+		
+		JLabel labelEmail = new JLabel("이메일");
+		labelEmail.setOpaque(true);
+		labelEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		labelEmail.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		labelEmail.setBackground(SystemColor.activeCaption);
+		labelEmail.setBounds(12, 158, 160, 64);
+		frame.getContentPane().add(labelEmail);
+		
+		textEmail = new JTextField();
+		textEmail.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		textEmail.setColumns(10);
+		textEmail.setBounds(184, 158, 348, 64);
+		frame.getContentPane().add(textEmail);
+		
+		JButton btnConfirm = new JButton("입력 완료");
+		btnConfirm.setFont(new Font("D2Coding", Font.PLAIN, 32));
+		btnConfirm.setBounds(12, 232, 240, 64);
+		frame.getContentPane().add(btnConfirm);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("D2Coding", Font.PLAIN, 24));
+		textArea.setBounds(12, 306, 520, 204);
+		frame.getContentPane().add(textArea);
+	}
+}
