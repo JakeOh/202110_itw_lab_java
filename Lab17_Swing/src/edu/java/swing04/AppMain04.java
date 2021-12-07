9dbe5b4eb7ba5df3ea06be95a67ec4ba5f38fe5f
Jake Oh <jake.oh.lecture@gmail.com>
2021-12-07 12:09:30 +0900

    .

--- Lab17_Swing/src/edu/java/swing04/AppMain04.java
+++ Lab17_Swing/src/edu/java/swing04/AppMain04.java
@@ -6,10 +6,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AppMain04 {
 
 	private JFrame frame;
+	private JLabel labelMessage;
 
 	/**
 	 * Launch the application.
@@ -43,30 +46,52 @@ public class AppMain04 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel labelMessage = new JLabel("New label");
+		labelMessage = new JLabel("New label");
 		labelMessage.setFont(new Font("D2Coding", Font.BOLD, 32));
 		labelMessage.setBounds(12, 10, 410, 61);
 		frame.getContentPane().add(labelMessage);
 		
 		JButton btn1 = new JButton("익명클래스");
+		// ActionListener 객체를 익명 클래스 객체로 생성하고, 버튼에 이벤트 핸들러를 등록.
+		btn1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				labelMessage.setText(e.getActionCommand());
+			}
+		});
 		btn1.setFont(new Font("D2Coding", Font.PLAIN, 24));
 		btn1.setBounds(12, 81, 190, 61);
 		frame.getContentPane().add(btn1);
 		
 		JButton btn2 = new JButton("람다표현식");
+		// ActionListener 익명 클래스 객체 생성 -> 람다 표현식(Lambda expression): (param) -> { 구현 코드 작성; }
+		btn2.addActionListener((e) -> {
+			labelMessage.setText(e.getActionCommand());
+		});
 		btn2.setFont(new Font("D2Coding", Font.PLAIN, 24));
 		btn2.setBounds(214, 81, 190, 61);
 		frame.getContentPane().add(btn2);
 		
 		JButton btn3 = new JButton("메서드 1");
+		btn3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				performButtonEvent(e);
+			}
+		});
 		btn3.setFont(new Font("D2Coding", Font.PLAIN, 24));
 		btn3.setBounds(12, 152, 190, 61);
 		frame.getContentPane().add(btn3);
 		
-		JButton btn4 = new JButton("메서드2");
+		JButton btn4 = new JButton("메서드 2");
+		btn4.addActionListener((e) -> { performButtonEvent(e); });
 		btn4.setFont(new Font("D2Coding", Font.PLAIN, 24));
 		btn4.setBounds(214, 152, 190, 61);
 		frame.getContentPane().add(btn4);
+	}
+
+	private void performButtonEvent(ActionEvent e) {
+		labelMessage.setText(e.getActionCommand());
 	}
 
 }
